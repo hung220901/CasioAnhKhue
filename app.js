1a8fb2b1c0c6ae7f5399a9a15621cd2aa514fdf0
hung220901 <hung220901@gmail.com>
2022-03-16 22:59:13 +0700

    up

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ $(document).ready(function(){
         dots: true,
     });
   });
+
+  $(document).ready(function(){
+    $('.product-slider').slick({
+        autoplay:"true",
+        autoplaySpeed: 2000,
+        arrows: false,
+    
+    });
+  });
+
+  $(document).ready(function(){
+    $('.style-product-slider').slick({
+        autoplay:"true",
+        autoplaySpeed: 2000,
+        arrows: false,
+    
+    });
+  });
